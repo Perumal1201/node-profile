07d6b7d203645e8e4b52ff18ddb217a4bf3ee6fb
LAPTOP-G2CTBK3G\perum <perumals1201@live.com>
2018-06-27 23:49:16 +0530

    merge fix

--- profileaddsearch/index.js
+++ profileaddsearch/index.js
@@ -2,21 +2,13 @@ var express = require('express');
 var app = express();
 
 var register = require('./routes/register.js');
+var search = require('./routes/search.js');
 
 app.get('/', function(req, res){
     res.send('This is home page');
 });
 
-<<<<<<< HEAD
-var search = require('./routes/search');
-app.get('/search', function(req, res){
-    res.send('This is home page');
-});
-
-
-app.listen(3000);
-=======
 app.use('/register', register);
+app.use('/search', search);
 
 app.listen(3000);
->>>>>>> e5206d2e8f32169be45e2ee0953652cdfb3d6e79
