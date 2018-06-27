5a8578d330e92dde4b77323a6179d9e0c05eb87f
vasuleo <vasudevan1606@gmail.com>
2018-06-27 23:27:40 +0530

    modified register

--- profileaddsearch/routes/register.js
+++ profileaddsearch/routes/register.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
+router.get('/home', function(req, res){
     res.send('This is register page');
 });
 
