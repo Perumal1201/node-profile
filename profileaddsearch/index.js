e5206d2e8f32169be45e2ee0953652cdfb3d6e79
LAPTOP-G2CTBK3G\perum <perumals1201@live.com>
2018-06-27 23:42:41 +0530

    registerd

--- profileaddsearch/index.js
+++ profileaddsearch/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
-var app = express;
+var app = express();
 
-var register = require('./routes/register');
+var register = require('./routes/register.js');
+
 app.get('/', function(req, res){
     res.send('This is home page');
 });
+
+app.use('/register', register);
 
 app.listen(3000);
