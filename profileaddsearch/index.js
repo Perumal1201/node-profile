7daefdd6b1499164a9e483eb4e69ce78902f55ef
vasuleo <vasudevan1606@gmail.com>
2018-06-27 23:43:18 +0530

    search done

--- profileaddsearch/index.js
+++ profileaddsearch/index.js
@@ -6,4 +6,10 @@ app.get('/', function(req, res){
     res.send('This is home page');
 });
 
+var search = require('./routes/search');
+app.get('/search', function(req, res){
+    res.send('This is home page');
+});
+
+
 app.listen(3000);
