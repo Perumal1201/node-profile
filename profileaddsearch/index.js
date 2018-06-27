6af19a24677a58d7a76f090ad93355d4c6d10834
LAPTOP-G2CTBK3G\perum <perumals1201@live.com>
2018-06-28 00:05:41 +0530

    serch ui ready.

--- profileaddsearch/index.js
+++ profileaddsearch/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views','./views');
+
 var register = require('./routes/register.js');
 var search = require('./routes/search.js');
 
